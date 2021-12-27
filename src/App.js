aa8edb799bebfe83b7dced3c0dd9d2859fc5ec79
Byeolikim <213069@naver.com>
2021-12-27 14:01:16 +0900

    세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from './Hello';
+
+//컴포넌트는 일종의 UI 조각입니다
+//그리고 쉽게 재사용할 수 있습니다
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
   );
 }
