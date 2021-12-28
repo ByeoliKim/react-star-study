b976575aefddc103f1e0dcf78077988cbf30f7a1
Byeolikim <213069@naver.com>
2021-12-28 17:05:23 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-function Wrapper () {
+function Wrapper ({ children }) {
     const style = {
         border: '2px solid black',
         padding: '16px',
     };
     return (
         <div style={style}>
-
+            { children }
         </div>
     )
 }
+//내부의 내용을 보여지게 하기 위해서는 Wrapper 에서 props.children 을 렌더링 해 줘야 합니다.
+
+//쓰이는 곳 (App.js) 에서 값을 정한다 = props (부모)
+//쓰임 당하는 곳 (Hello.js 나 Wrapper.js) 에서 값을 정한다 = children
 
 export default Wrapper
