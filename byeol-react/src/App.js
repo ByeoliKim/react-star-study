df317f46851097732e235c50efeee89174b911df
Byeolikim <213069@naver.com>
2022-01-06 18:00:24 +0900

    input 상태 관리

--- byeol-react/src/App.js
+++ byeol-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './inputSample';
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
       <Hello name="react" color="blue" isSpecial={true} />
       <Hello color="pink"/>
     </Wrapper>
-    */
+    
     <Counter />
+    */
+    <InputSample />
   );
 }
 
